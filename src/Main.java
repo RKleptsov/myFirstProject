6328c49aec9f0d5d48e49503e83790eb28e4ec57
Rustam <rklepa_20@mail.ru>
2023-05-03 19:32:46 +0300

    Созданы 4 функции, каждая принимает 2 числа и производит арифметическую операцию.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        summa(7,5);
+        vichit(9,1);
+        ymnoj(3,4);
+        delen(48,6);
+    }
+    public static void summa(int a,int b){
+        int r = a+b;
+        System.out.println(r);
+    }
+    public static void vichit(int a,int b){
+        int r = a-b;
+        System.out.println(r);
+    }
+    public static void ymnoj(int a,int b){
+        int r = a*b;
+        System.out.println(r);
+    }
+    public static void delen(int a,int b){
+        int r = a/b;
+        System.out.println(r);
     }
 }
