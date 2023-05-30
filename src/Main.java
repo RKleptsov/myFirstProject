21d3992821895e0d66a54f5a5148b5f70b995454
Rustam <rklepa_20@mail.ru>
2023-05-30 09:59:08 +0300

    1) добавлена возможность делить числа, которые не делятся друг на друга без остатка. (когда числа не делятся друг на друга - возвращается частное от операции, без остатка) 2) вынесена логика калькулятора в отдельный класс. 3) вызваны все методы экземпляра из класса main. 4) тестовые наборы вынесены в отдельный класс без конструктора. 5) переделаны тест.наборы.

--- src/Main.java
+++ src/Main.java
@@ -1,58 +1,30 @@
+import java.sql.SQLOutput;
 public class Main {
     public static void main(String[] args) {
 
-    System.out.println(info("Рустам", "Клепцов"));
-    System.out.println(summa(7, 5));
-    System.out.println(vichit(9, 1));
-    System.out.println(ymnoj(3, 4));
-    System.out.println(delen(48, 6));
+      Calculator calculator = new Calculator();
 
-    int[] array1 = new int[] {7,9,3,48};
-    int[] array2 = new int[] {5,1,4,6};
-    int[] array3 = new int[] {10,50,100,11};
-    int[] array4 = new int[] {2,5,25,1};
-    int[] array5 = new int[] {57,22,31,5};
-    int[] array6 = new int[] {43,21,9,2};
-    int[] array7 = new int[] {96,90,102,77};
-    int[] array8 = new int[] {16,15,2,7};
+        for (int i = 0; i < TestNabor.array1.length; i++) {}
+        System.out.print(Calculator.summa(TestNabor.array1[1],TestNabor.array1[0])+" ");
+        System.out.println(Calculator.summa(TestNabor.array1[3], TestNabor.array1[2]));
 
-        for (int i=0; i < array1.length;i++) {
-            System.out.print(summa(array1[i],array2[i])+" ");
-        }
-        System.out.println();
-
-        for (int i=0; i<array3.length;i++) {
-            System.out.print(ymnoj(array3[i],array4[i])+" ");
-        }
-        System.out.println();
+        for (int i = 0; i < TestNabor.array2.length; i++) {}
+        System.out.print(Calculator.ymnoj(TestNabor.array2[2], TestNabor.array2[0])+" ");
+        System.out.println(Calculator.ymnoj(TestNabor.array2[3], TestNabor.array2[1]));
 
         int i = 0;
-        while (i<array5.length) {
-            System.out.print(vichit(array5[i],array6[i])+" ");
+        while (i < TestNabor.array3.length) {
             i++;
         }
-        System.out.println();
+        System.out.print(Calculator.vichit(TestNabor.array3[1], TestNabor.array3[0]) + " ");
+        System.out.println(Calculator.vichit(TestNabor.array3[2],TestNabor.array3[3]));
 
         int ii = 0;
-        while (ii<array7.length) {
-            System.out.print(delen(array7[ii],array8[ii])+" ");
+        while (ii < TestNabor.array4.length) {
             ii++;
         }
+        System.out.print(Calculator.delen(TestNabor.array4[0], TestNabor.array4[1]) + " ");
+        System.out.println(Calculator.delen(TestNabor.array4[2],TestNabor.array4[3]));
     }
-    static int summa(int a,int b) {
-        return a + b;
-    }
-    static int vichit(int a,int b) {
-        return a - b;
-    }
-    static int ymnoj(int a,int b) {
-        return a * b;
-    }
-    static int delen(int a,int b) {
-        return a / b;
-    }
-    static String info(String text1,String text2) {
-        return text1 +" Sudjuk "+ text2;
-    }
-
 }
+
---
+++ src/TestNabor.java
@@ -0,0 +1,7 @@
+public class TestNabor {
+    public static int[] array1 = new int[]{ 7, 8, 3, 48};
+    public static int[] array2 = new int[] {5, 1, 4, 6};
+    public static int[] array3 = new int[] {11, 50, 100, 13};
+    public static int[] array4 = new int[] {30, 7, 20, 1};
+
+    }
