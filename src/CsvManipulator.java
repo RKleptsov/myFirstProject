572a4421b9e294f92120482a24c7e0469b25a30b
Rustam <rklepa_20@mail.ru>
2023-11-20 04:07:11 +0300

    инструмент для CSV файлов 1. Добавлен класс csvManipulator 2. и методы: чтение, создание, изменение строки, поиск, удаление и переименование CSV файла. 3. добавлены новые ошибки: не найден файл, не найдена строка.

--- src/CsvManipulator.java
+++ src/CsvManipulator.java
@@ -1,10 +1,16 @@
 import java.io.*;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 
 public class CsvManipulator {
-    public void readCsvFile(String filePath) {
+    private static final String FILE_PATH = "E://JavaFiles//";
+    private String getFilePath(String name) {
+        return FILE_PATH + name;
+    }
+        public void readCsvFile(String name) {
+        String filePath = getFilePath(name);
         String line = "";
         String splitBy = ",";
         try {
@@ -19,7 +25,8 @@ public class CsvManipulator {
     }
     public boolean createNewCsv (String fileName) {
         try {
-            FileWriter fileWriter = new FileWriter(fileName);
+            String filePath = getFilePath(fileName);
+            FileWriter fileWriter = new FileWriter(filePath);
             fileWriter.append("raz,dva,tri\n");
             fileWriter.close();
             System.out.println("ксв файл создан");
@@ -29,9 +36,10 @@ public class CsvManipulator {
         }
         return false;
     }
-    public String filePath = "E://JavaFiles//new_file.csv";
-    public boolean updateCsv(int columnToUpdate, int lineToUpdate, String valueToUpdate) {
+
+    public boolean updateCsv(String name, int columnToUpdate, int lineToUpdate, String valueToUpdate) {
         try {
+            String filePath = getFilePath(name);
             List<String> lines = Files.readAllLines(Paths.get(filePath));
             if (lineToUpdate >= 0 && lineToUpdate < lines.size()) {
                 String[] columns = lines.get(lineToUpdate).split(",");
@@ -49,10 +57,11 @@ public class CsvManipulator {
         }
         return false;
     }
-    public boolean deleteCsv(String filePath) {
-        File file = new File(filePath);
+    public boolean deleteCsv(String name) {
         try {
-            file.delete();
+            String filePath = getFilePath(name);
+            Path path = Paths.get(filePath);
+            Files.delete(path);
             System.out.println("ксв файл удален");
             return true;
         } catch (Exception e) {
@@ -60,10 +69,11 @@ public class CsvManipulator {
         }
         return false;
     }
-    public boolean editCsv(String newFilePath) {
-        File currentFile = new File(filePath);
-        File newFile = new File(newFilePath);
+    public boolean editCsv(String newName, String name) {
         try {
+            String filePath = getFilePath(name);
+            File currentFile = new File(filePath);
+            File newFile = new File(getFilePath(newName));
             currentFile.renameTo(newFile);
             System.out.println("ксв файл переименован");
             return true;
@@ -72,8 +82,10 @@ public class CsvManipulator {
         }
         return false;
     }
-    public boolean searchCsvFile(String fileName) {
-        File file = new File(fileName);
+
+    public boolean searchCsvFile(String name) {
+        String filePath = getFilePath(name);
+        File file = new File(filePath);
         try {
             if (!file.exists()) {
                 throw new Errors(Enum.FileNotFound.getError());
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Stack;
 public class Main {
   public static void main(String[] args) {
     DataProvider data = new DataProvider();
@@ -9,42 +6,42 @@ public class Main {
     data.setStr(generator.generatorStroki(10));
 
     CsvManipulator csvManipulator = new CsvManipulator();
-    csvManipulator.readCsvFile("E://JavaFiles//aaff.csv");
-    csvManipulator.createNewCsv("E://JavaFiles//new_file.csv");
-    csvManipulator.updateCsv(0, 0, "1");
-    csvManipulator.editCsv("E://JavaFiles//newFFF.csv");
-    csvManipulator.searchCsvFile("E://JavaFiles//newFFF.csv");
-    csvManipulator.deleteCsv("E://JavaFiles//new_file.csv");
+    csvManipulator.readCsvFile("aaff.csv");
+    csvManipulator.createNewCsv("new_file.csv");
+    csvManipulator.updateCsv("new_file.csv", 0, 0, "1");
+    csvManipulator.editCsv(generator.generatorStroki(3) +".csv", "new_file.csv");
+    csvManipulator.searchCsvFile("aaff.csv");
+    csvManipulator.deleteCsv("new_file.csv");
 
-    try {
-      digits.numberDigits(data.getStr());
-    } catch (Errors e) {
-      System.out.println("ПОЙМАЛ: " + e.getMessage());
-    }
-    Stack<Character> stack = new Stack<>();
-    List<Character> nonDigits = new ArrayList<>();
-    int errorCount = 0;
-    for (int i = 9; i >= 0; ) {
-      try {
-        char generatedChar = generator.generateRandomCharacter();
-        int digitValue = Character.getNumericValue(generatedChar);
-        if (digitValue != i) {
-          nonDigits.add(generatedChar);
-          throw new Errors(Enum.NeTotSimvol.getError());
-        } else {
-          stack.push(generatedChar);
-          i--;
-        }
-      } catch (Errors e) {
-        System.out.println("ПОЙМАЛ " + e.getMessage());
-        errorCount++;
-      }
-    }
-    while (!stack.isEmpty()) {
-      System.out.print(stack.pop() + " ");
-    }
-    int size = nonDigits.size();
-    assert errorCount == size;
+//    try {
+//      digits.numberDigits(data.getStr());
+//    } catch (Errors e) {
+//      System.out.println("ПОЙМАЛ: " + e.getMessage());
+//    }
+//    Stack<Character> stack = new Stack<>();
+//    List<Character> nonDigits = new ArrayList<>();
+//    int errorCount = 0;
+//    for (int i = 9; i >= 0; ) {
+//      try {
+//        char generatedChar = generator.generateRandomCharacter();
+//        int digitValue = Character.getNumericValue(generatedChar);
+//        if (digitValue != i) {
+//          nonDigits.add(generatedChar);
+//          throw new Errors(Enum.NeTotSimvol.getError());
+//        } else {
+//          stack.push(generatedChar);
+//          i--;
+//        }
+//      } catch (Errors e) {
+//        System.out.println("ПОЙМАЛ " + e.getMessage());
+//        errorCount++;
+//      }
+//    }
+//    while (!stack.isEmpty()) {
+//      System.out.print(stack.pop() + " ");
+//    }
+//    int size = nonDigits.size();
+//    assert errorCount == size;
   }
 }
 
